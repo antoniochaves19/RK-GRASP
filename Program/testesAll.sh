b79f503cb3fc8450dc6277dfa5708dfb5e2a9532
Antonio Augusto Chaves <74926331+antoniochaves19@users.noreply.github.com>
2024-05-29 16:19:35 -0300

    Delete Program/testesAll.sh

--- Program/testesAll.sh
+++
@@ -1,7 +0,0 @@
-#arquivo_de_lote
-#!/bin/bash
-./runTestTSP "/home/chaves/Projetos/Chaves/RKO-GRASP/Instances/TSP-tests-run.csv" 4 0
-./runTestTHLP "/home/chaves/Projetos/Chaves/RKO-GRASP/Instances/THLP-tests-run.csv" 4 0
-./runTestSTCP "/home/chaves/Projetos/Chaves/RKO-GRASP/Instances/STCP-tests-run.csv" 4 0
-./runTestHMP "/home/chaves/Projetos/Chaves/RKO-GRASP/Instances/HMP-tests-run.csv" 4 0
-./runTestMTSP "/home/chaves/Projetos/Chaves/RKO-GRASP/Instances/MTSP-tests-run.csv" 4 0
